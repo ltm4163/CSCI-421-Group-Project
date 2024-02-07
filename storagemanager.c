a067fc39276629dfc8aeb0771b4ad439d8fbfee3
James <jm1060@rit.edu>
2024-02-07 14:23:48 -0500

    Started working on the getting the files on a certain table.

--- storagemanager.c
+++ storagemanager.c
@@ -2,3 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include "catalog.h"
+
+struct Record * getrecords(Catalog catalog)
+{
+    int tablenumber;
+    printf("Enter a table number: ");
+    scanf("%d", &tablenumber);
+    if(tablenumber < 0 && tablenumber >= sizeof(catalog->tables)){
+        printf("Invalid input try again: ");
+        scanf("%d")
+    }
+}
+
+void insertrecord()
+{
+
+}
