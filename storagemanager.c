d1aa187915ca2d585b20d8dda6917162a5f1672a
James <jm1060@rit.edu>
2024-02-07 14:55:37 -0500

    Probably finished with getting recrods from a certain table number.

--- storagemanager.c
+++ storagemanager.c
@@ -8,10 +8,13 @@ struct Record * getrecords(Catalog catalog)
     int tablenumber;
     printf("Enter a table number: ");
     scanf("%d", &tablenumber);
-    if(tablenumber < 0 && tablenumber >= sizeof(catalog->tables)){
+    while(tablenumber < 0 && tablenumber >= sizeof(catalog->tables)){
         printf("Invalid input try again: ");
-        scanf("%d")
+        scanf("%d",tablenumber);
     }
+    Table table=Catalog->tables[tablenumber];
+    Record * returnrecords=table->records;
+    return returnrecords;
 }
 
 void insertrecord()
