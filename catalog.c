91741f7ddd52a42e6902db0d4eaf36ecbbaa6ce0
Cole Allen <cda9685@g.rit.edu>
2024-02-06 22:02:58 -0500

    Implemented the basic struct system for catalog

--- catalog.c
+++ catalog.c
@@ -1,1 +1,2 @@
+#include "catalog.h"
 
---
+++ catalog.h
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_PAGE_SIZE 4096  // bytes
+#define MAX_NUM_RECORDS 100  // Can change
+#define MAX_NUM_ATTRIBUTES 50  // Can also change
+#define MAX_NAME_SIZE 50  // "   "
+
+struct Catalog {
+    struct Table* tables;
+    int tableCount;
+    struct BufferPool* bufferPool;
+};
+
+struct Table {
+    struct Record records[MAX_NUM_RECORDS];
+    struct Attribute attributes[MAX_NUM_ATTRIBUTES];
+    char name[MAX_NAME_SIZE];
+    int recordCount;
+    int currentPage;
+};
+
+struct Record {
+    int id;
+    char name[MAX_NAME_SIZE];
+
+};
+
+struct BufferPool{
+    struct Page* pages;
+    int pageCount;
+};
+
+struct Page {
+    char data[MAX_PAGE_SIZE];
+};
+
+struct Attribute {
+    char name[MAX_NAME_SIZE];
+    char type[20];
+    // Add constraints or other things. Maybe size
+};
