d853205cb9bce8e073e4fa3f6b31f52b7d89229d
lmercuro702 <ltm4163@g.rit.edu>
2024-02-20 17:37:27 -0500

    Added comments to ParseAttribute. Still UNTESTED

--- parse.c
+++ parse.c
@@ -6,26 +6,42 @@
 #include "attribute.h"
 #include "table.h"
 
+/*	
+* Method: ParseAttribute
+* Helper function to parse attribute input
+*
+* @param char* attributes: string consisting of attribute input
+* 							Ex: (foo int primarykey, bar string notnull)
+* @return returns a list of attribute schemas 
+*/
 AttributeSchema* ParseAttribute(char* attributes) {
 	int attribute_count = 0;
 	char *ptr1, *ptr2;
+	// holds name of attribute
 	char* name[MAX_NAME_SIZE];
+	// holds type of attribute
 	char* type[MAX_NAME_SIZE];
+	// holds contraints, this must be PARSED 
 	char* constraints[MAX_NAME_SIZE];
-	AttributeSchema* attribute_list[MAX_NUM_ATTRIBUTES];
+	// list of attributes to return
+	AttributeSchema* attribute_arr[MAX_NUM_ATTRIBUTES];
 
+	// uses strtok_r so for nested parsing 
 	char* attr_tok = strtok_r(attributes, ",", &ptr1);
 	while(attr_tok != NULL) {
+		// current attribute being parsed
 		AttributeSchema* cur_attribute = malloc(sizeof(AttributeSchema)); 
 
+		// parses name, type, and constraints 
 		sscanf(attr_tok, " %50s %19s %19[^,]", name, type, constraints);
 
+		// puts name and type into attribute
 		strcpy(cur_attribute->name, name);
 		strcpy(cur_attribute->type, type);
 
-		// TODO implement logic for type of attribute
-
+		// tokenizes constraints
 		char* const_tok = strtok_r(constraints, " ", &ptr2);
+		// loops through constraints, flips constraint flag if constraint found
 		while(const_tok != NULL) {
 			if(strcmp(const_tok, "notnull") == 0) {
 				cur_attribute->nonNull = true;
@@ -37,12 +53,15 @@ AttributeSchema* ParseAttribute(char* attributes) {
 			const_tok = strtok_r(NULL, " ", &ptr2);
 		}
 
-		attribute_list[attribute_count] = cur_attribute;
+		// adds attribute to array
+		attribute_arr[attribute_count] = cur_attribute;
+		// increments count
 		attribute_count++;
+		// gets next attribute
 		attr_tok = strtok_r(NULL, ",", &ptr1);
 	}
 
-	return attribute_list;
+	return attribute_arr;
 }
 
 TableSchema* ParseTable(char* table_name, char* attributes) {
