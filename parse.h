f0047fbf0ff109737316932ed848779e5063a728
lmercuro702 <ltm4163@g.rit.edu>
2024-02-20 18:19:38 -0500

    updated definitions in parse.h (subject to change)

--- parse.h
+++ parse.h
@@ -1,6 +1,14 @@
 #ifndef PARSE_H_
 #define PARSE_H_
 
+// parses attributes
+AttributeSchema* ParseAttribute(char* attributes);
+
+// parses table
+TableSchema* ParseTable();
+
+
+// main parser
 void parse();
 
 
