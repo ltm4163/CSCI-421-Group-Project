740a5e3c3a556b095bf3df2eebbf4432384a6b89
Alek Michelson <am7590@rit.edu>
2024-02-19 22:17:52 -0500

    remove findPage from buffer

--- bufferpool.c
+++ bufferpool.c
@@ -8,12 +8,12 @@ void initializeBufferPool(BufferPool* bP) {
 
 // Search functions:
 
-void findPage(BufferPool* bP, int tableNumber, int pageNumber) {
-    for (int i = 0; i < bP -> pageCount; i++) {
-        Page page = bP -> pages[i*sizeof(Page)];
-        if (page.tableNumber == tableNumber && page.pageNumber == pageNumber) {
-            return &bP -> pages[i*sizeof(Page)];
-        }
-    }
-    return NULL;
-}  // Do we want to implement this to search for data instead?
+//void findPage(BufferPool* bP, int tableNumber, int pageNumber) {
+//    for (int i = 0; i < bP -> pageCount; i++) {
+//        Page page = bP -> pages[i*sizeof(Page)];
+//        if (page.tableNumber == tableNumber && page.pageNumber == pageNumber) {
+//            return &bP -> pages[i*sizeof(Page)];
+//        }
+//    }
+//    return NULL;
+//}  // Do we want to implement this to search for data instead?
