e19a39e576c417565bc1fcf00e65abeb2946d1a5
lmercuro702 <ltm4163@g.rit.edu>
2024-02-20 23:48:36 -0500

    new main file

--- main.h
+++ main.h
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "catalog.h"
-#include "bufferpool.h"
+#include "buffer.h"
+#include "parse.h"
+
+#ifndef MAIN_H
+#define MAIN_H
 
 void initializeDatabase(const char* dbLocation, int pageSize, int bufferSize);
 Catalog *getCatalog(); //returns catalog variable to other files
-BufferPool *getBufferPool(); //returns buffer pool variable to other files
+Buffer *getBuffer(); //returns buffer pool variable to other files
+
+#endif
