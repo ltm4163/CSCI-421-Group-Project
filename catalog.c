6158844b5724e2bbe179d465469ff39043f52c21
Cole Allen <cda9685@g.rit.edu>
2024-02-21 01:31:22 -0500

    Comleted read/write functions for catelog; implemented .gitignore

--- catalog.c
+++ catalog.c
@@ -25,3 +25,29 @@ void addPage(Catalog* c) {
 void addAttribute(Catalog* c, char name[MAX_NAME_SIZE], char type[20]) {
 
 }  // Add more constraints as needed
+
+void writeCatalogToFile(Catalog* c, char* pathname) {
+    FILE* file = fopen(pathname, "wb");
+
+    fwrite(c, sizeof(Catalog), 1, file);
+
+    fwrite(c -> tables, sizeof(TableSchema), 1, file);
+
+    fwrite(c -> tables -> attributes, sizeof(AttributeSchema), 1, file);
+
+    fclose(file);
+}
+
+void readCatalogFromFile(Catalog* c, char* pathname) {
+    FILE* file = fopen(pathname, "rb");
+
+    fread(c, sizeof(Catalog), 1, file);
+
+    c -> tables = (TableSchema*)malloc(sizeof(TableSchema));
+    fread(c -> tables, sizeof(TableSchema), 1, file);
+
+    c -> tables -> attributes = (AttributeSchema*)malloc(sizeof(AttributeSchema));
+    fread(c -> tables -> attributes, sizeof(AttributeSchema), 1, file);
+
+    fclose(file);
+}
--- catalog.h
+++ catalog.h
@@ -19,4 +19,14 @@ void addPage(Catalog* c);  // How do we deal with this one?
 
 void addAttribute(Catalog* c, char name[MAX_NAME_SIZE], char type[20]);  // Add more constraints as needed
 
+/// @brief Writes the contents of the provided Catalog pointer to the provided pathname
+/// @param c The catalog with data to write (everything must be initialized/malloc'd)
+/// @param pathname The path of the .bin file to write to (if it does not exist, it will be created)
+void writeCatalogToFile(Catalog* c, char* pathname);
+
+/// @brief Reads the contents in the provided pathname to the provided Catalog pointer
+/// @param c The catalog to receive the data (everything must be initialized/malloc'd)
+/// @param pathname The path of the file
+void readCatalogFromFile(Catalog* c, char* pathname);
+
 #endif
